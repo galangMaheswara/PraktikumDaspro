c148aca85a24e6d0b886792e2624d6206f41874a
galangMaheswara <ullyses88@gmail.com>
2025-10-02 08:33:45 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS25.java
+++ jobsheet5/ifCetakKRS25.java
@@ -13,7 +13,7 @@ public class ifCetakKRS25 {
         System.out.println("Pembayaran UKT terverifikasi ");
         System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
     } else {
-        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu ");
 
     }
 
