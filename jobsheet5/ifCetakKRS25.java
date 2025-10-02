89c1ebd7dc0b29c68ec157593eab93aeb4280933
galangMaheswara <ullyses88@gmail.com>
2025-10-02 10:36:03 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS25.java
+++ jobsheet5/ifCetakKRS25.java
@@ -9,14 +9,9 @@ public class ifCetakKRS25 {
     System.out.print("Apakah UKT sudah lunas? (true/false): ");
     boolean uktLunas = sc.nextBoolean();
 
-    if (uktLunas) {
-        System.out.println("Pembayaran UKT terverifikasi ");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-    } else {
-        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu ");
-
-    }
-
+    String pesan = uktLunas 
+            ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+    System.out.println(pesan);
     sc.close();
     }
 }
